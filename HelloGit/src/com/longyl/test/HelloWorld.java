fbd2a7e42426f7625378a18ab01fbea66908aa16
dacheng <pumpkinjugg@gmail.com>
2012-10-12 22:30:04 +0800

    这是我的第一个git项目！

--- HelloGit/src/com/longyl/test/HelloWorld.java
+++ HelloGit/src/com/longyl/test/HelloWorld.java
@@ -2,6 +2,6 @@ package com.longyl.test;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.print("Hello World!Welcome to github!!");
+		System.out.print("这是我的第一个git项目!");
 	}
 }
