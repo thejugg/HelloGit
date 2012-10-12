98c74f62a7e7ef7be79da0778ad771b45f5017cf
LYL <pumpkinjugg@gmail.com>
2012-10-12 22:46:46 +0800

    002

--- HelloGit/src/com/longyl/test/HelloWorld.java
+++ HelloGit/src/com/longyl/test/HelloWorld.java
@@ -2,7 +2,8 @@ package com.longyl.test;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.print("这是我的第一个git项目!");
+		System.out.print("This is my 1st git project!");
 		System.out.print("001");
+		System.out.print("002");
 	}
 }
